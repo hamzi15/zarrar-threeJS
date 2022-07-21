23d56b3836a4d097e4a7706ff6a20188bc72da57
Ahmad Saad <64619851+saadpocalypse@users.noreply.github.com>
2022-07-22 00:46:36 +0500

    Update camera.js

--- Components/camera.js
+++ Components/camera.js
@@ -2,6 +2,6 @@ import * as THREE from 'three';
 
 export function createCamera() {
 	const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 10000 );
-	camera.position.set( 0, 0, 500 );
+	camera.position.set( 0, 0, 600 );
 	return camera;
 }
