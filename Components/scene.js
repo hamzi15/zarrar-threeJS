f4220e3a7ef7219e738e209593df4114f035a027
ss10ss10 <sulaiman.sultan96@gmail.com>
2022-07-22 20:10:20 +0500

    fog enabled

--- Components/scene.js
+++ Components/scene.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 export function createScene() {
     let scene = new THREE.Scene();
     scene.background = new THREE.Color( 0x3d3d3d );
-    scene.fog = new THREE.FogExp2( 0x3d3d3d, 0.00041 );
+    scene.fog = new THREE.FogExp2( 0x3d3d3d, 0.00071 );
 
     return scene;
 }
